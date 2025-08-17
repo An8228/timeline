a5be41b7c898739bdc3e1c4917694c519b1e337c
An8228 <ananya.ms2005@gmail.com>
2025-08-17 21:56:29 +0530

    Delete fetcher.ts

--- fetcher.ts
+++
@@ -1,7 +0,0 @@
-import { FashionEvent } from "./types";
-
-export async function fetchEvents(): Promise<FashionEvent[]> {
-  const response = await fetch("data/events.json");
-  if (!response.ok) throw new Error("Failed to fetch events");
-  return response.json();
-}
